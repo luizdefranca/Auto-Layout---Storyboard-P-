9cc699a972b8ef26a7ca3e0d099489de8f89516a
LuizRamos <luizramospe@hotmail.com>
2019-05-07 17:58:03 -0400

    red and orange boxes done

---
+++ AutoLayout-Storyboard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayout-Storyboard
+//
+//  Created by Luiz on 5/7/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayout-StoryboardTests/AutoLayout_StoryboardTests.m
@@ -0,0 +1,37 @@
+//
+//  AutoLayout_StoryboardTests.m
+//  AutoLayout-StoryboardTests
+//
+//  Created by Luiz on 5/7/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AutoLayout_StoryboardTests : XCTestCase
+
+@end
+
+@implementation AutoLayout_StoryboardTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
